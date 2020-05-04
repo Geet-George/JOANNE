89976669b9783cd7046f098d44a63abdb225cbe0
Geet George <geetgeorge12@gmail.com>
2020-05-04 18:23:47 +0200

    add install dependencies

--- setup.py
+++ setup.py
@@ -20,4 +20,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.18.0",
+        "xarray>=0.15.0",
+        "netCDF4>=1.5.0",
+        "MetPy>=0.12.1",
+        "scikit-learn>=0.22.0",
+        "PyYAML>=5.3.0",
+    ],
 )
