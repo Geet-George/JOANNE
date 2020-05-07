a0e4c9d44bb8bb8274663063e7f1b59c3297459b
Geet George <geetgeorge12@gmail.com>
2020-05-07 12:32:05 +0200

    rename lv3 and lv4 files

--- joanne/Level_3/Level_3.py
+++ joanne/Level_3/Level_3.py
@@ -8,7 +8,7 @@ import numpy as np
 import xarray as xr
 
 import dicts
-import fn_3a as f3a
+import fn_3 as f3
 import joanne
 
 warnings.filterwarnings(
@@ -17,7 +17,7 @@ warnings.filterwarnings(
 # %%
 lv2_data_directory = "/Users/geet/Documents/JOANNE/Data/code_testing_data/"  # Level_2/"  # code_testing_data/"
 
-lv3_dataset = f3a.lv3_structure_from_lv2(lv2_data_directory)
+lv3_dataset = f3.lv3_structure_from_lv2(lv2_data_directory)
 
 # %%
 
@@ -36,12 +36,12 @@ sounding = lv3_dataset.sounding.values
 to_save_ds = xr.Dataset(coords={"height": obs, "sounding": sounding})
 
 for var in dicts.list_of_vars:
-    f3a.create_variable(
+    f3.create_variable(
         to_save_ds, var, data=nc_data, dims=dicts.nc_dims, attrs=dicts.nc_attrs
     )
 
 file_name = (
-    "EUREC4A_JOANNE_Dropsonde-RD41_" + "Level_3A_v" + str(joanne.__version__) + ".nc"
+    "EUREC4A_JOANNE_Dropsonde-RD41_" + "Level_3_v" + str(joanne.__version__) + ".nc"
 )
 
 save_directory = (
