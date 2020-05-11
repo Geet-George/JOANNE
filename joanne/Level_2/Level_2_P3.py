af08a87d59e7de642534e477715d6f146c00d08a
Geet George <geetgeorge12@gmail.com>
2020-05-11 03:42:49 +0200

    create L2 files with f2, dicts and QC separate

--- joanne/Level_2/Level_2_P3.py
+++ joanne/Level_2/Level_2_P3.py
@@ -19,105 +19,107 @@ from joanne.Level_2 import dicts
 
 reload(f2)
 reload(dicts)
-# %%
-
-sonde_ds, directory, a_dir, logs_directory, a_files, file_time, sonde_paths = f2.get_all_sondes_list('P3')
 
 # %%
-a_filepaths = []
-# list to store individual file paths for all A files
+for Platform in ['HALO','P3'] :
 
-for i in a_files:
-    a_filepaths.append(sorted(glob.glob(a_dir + i + "*")))
+    sonde_ds, directory, a_dir, logs_directory, a_files, file_time, sonde_paths = f2.get_all_sondes_list(Platform)
 
-file_time_str = [None] * len(sonde_paths)
+    status_ds = xr.open_dataset(f'{logs_directory}Status_of_sondes_{Platform}_v0.5.1+1.g0c163c5.dirty.nc')
 
-for i in range(len(sonde_paths)):
-    file_time_str[i] = sonde_paths[i][-22:-7]
-# %%
-for i in tqdm(range(len(sonde_ds))):
+    a_filepaths = []
 
-    if status_ds.FLAG[i] == "GOOD":
+    for i in a_files:
+        a_filepaths.append(sorted(glob.glob(a_dir + i + "*")))
 
-        ht_indices = ~np.isnan(sonde_ds[i].alt)
-        # retrieving non-NaN indices of geopotential height (sonde_ds[i].alt)
-        # only time values at these indices will be used in Level-2 trajectory data;
-        # this means that only alternate u,v values are included in the Level-2 data
-        # PTU has 2 Hz measurement frequency, while GPS has a 4 Hz measurement frequency
+    file_time_str = [None] * len(sonde_paths)
 
-        ###----- Dimensions -----###
+    for i in range(len(sonde_paths)):
+        file_time_str[i] = sonde_paths[i][-22:-7]
 
-        obs = np.arange(1, ht_indices.sum() + 1, 1)
-        # creating the observations dimension of the NC file
+    for i in tqdm(range(len(sonde_ds))):
 
-        ###----- Variables -----###
+        if status_ds.FLAG[i] == "GOOD":
 
-        height = np.float32(sonde_ds[i].alt[ht_indices].values)
-        # Variable array: geopotential height
+            ht_indices = ~np.isnan(sonde_ds[i].alt)
+            # retrieving non-NaN indices of geopotential height (sonde_ds[i].alt)
+            # only time values at these indices will be used in Level-2 trajectory data;
+            # this means that only alternate u,v values are included in the Level-2 data
+            # PTU has 2 Hz measurement frequency, while GPS has a 4 Hz measurement frequency
 
-        time = sonde_ds[i].time[ht_indices].astype("float").values / 1e9
-        # Variable array: time
+            ###----- Dimensions -----###
 
-        wind_speed = np.float32(sonde_ds[i].wspd[ht_indices].values)
-        # Variable array: wind speed
+            obs = np.arange(1, ht_indices.sum() + 1, 1)
+            # creating the observations dimension of the NC file
 
-        wind_direction = np.float32(sonde_ds[i].wdir[ht_indices].values)
-        # Variable array: wind direction
+            ###----- Variables -----###
 
-        temperature = np.float32(sonde_ds[i].tdry[ht_indices].values)
-        # Variable array: temperature
+            height = np.float32(sonde_ds[i].alt[ht_indices].values)
+            # Variable array: geopotential height
 
-        pressure = np.float32(sonde_ds[i].pres[ht_indices].values)
-        # Variable array: pressure
+            time = sonde_ds[i].time[ht_indices].astype("float").values / 1e9
+            # Variable array: time
 
-        relative_humidity = np.float32(sonde_ds[i].rh[ht_indices].values)
-        # Variable array: relative humidity
+            wind_speed = np.float32(sonde_ds[i].wspd[ht_indices].values)
+            # Variable array: wind speed
 
-        latitude = np.float32(sonde_ds[i].lat[ht_indices].values)
-        # Variable array: latitude
+            wind_direction = np.float32(sonde_ds[i].wdir[ht_indices].values)
+            # Variable array: wind direction
 
-        longitude = np.float32(sonde_ds[i].lon[ht_indices].values)
-        # Variable array: longitude
+            temperature = np.float32(sonde_ds[i].tdry[ht_indices].values)
+            # Variable array: temperature
 
-        
-        ###--------- Creating and populating dataset --------###
+            pressure = np.float32(sonde_ds[i].pres[ht_indices].values)
+            # Variable array: pressure
 
-        to_save_ds = xr.Dataset(coords={"time": obs})
+            relative_humidity = np.float32(sonde_ds[i].rh[ht_indices].values)
+            # Variable array: relative humidity
 
-        for var in nc_meta.keys():
-            # v = var
-            create_variable(to_save_ds, var, eval(var))
+            latitude = np.float32(sonde_ds[i].lat[ht_indices].values)
+            # Variable array: latitude
 
-        file_name = (
-            "EUREC4A_JOANNE_"
-            + str(Platform_Name)
-            + "_Dropsonde-RD41_"
-            + file_time_str[i]
-            + "_v"
-            + str(joanne.__version__)
-            + ".nc"
-        )
-        save_directory = "/Users/geet/Documents/JOANNE/Data/Level_2/"
+            longitude = np.float32(sonde_ds[i].lon[ht_indices].values)
+            # Variable array: longitude
 
-        comp = dict(
-            zlib=True, complevel=4, fletcher32=True, _FillValue=np.finfo("float32").max
-        )
+            
+            ###--------- Creating and populating dataset --------###
 
-        encoding = {var: comp for var in to_save_ds.data_vars}
+            to_save_ds = xr.Dataset(coords={"time": obs})
 
-        nc_global_attrs = dicts.get_global_attrs(Platform,file_time[i],sonde_ds[i])
+            for var in dicts.nc_meta.keys():
+                # v = var
+                f2.create_variable(to_save_ds, var, eval(var))
 
-        for key in nc_global_attrs.keys():
-            to_save_ds.attrs[key] = nc_global_attrs[key]
+            file_name = (
+                "EUREC4A_JOANNE_"
+                + str(Platform)
+                + "_Dropsonde-RD41_"
+                + file_time_str[i]
+                + "_v"
+                + str(joanne.__version__)
+                + ".nc"
+            )
+            save_directory = "/Users/geet/Documents/JOANNE/Data/Level_2/"
 
-        flight_attrs = dicts.get_flight_attrs(a_filepaths[i][0])
+            comp = dict(
+                zlib=True, complevel=4, fletcher32=True, _FillValue=np.finfo("float32").max
+            )
 
-        for key in flight_attrs:
-            to_save_ds.attrs[key] = flight_attrs[key]
+            encoding = {var: comp for var in to_save_ds.data_vars}
 
-        ###--------- Saving dataset to NetCDF file --------###
+            nc_global_attrs = dicts.get_global_attrs(Platform,file_time[i],sonde_ds[i])
 
-        to_save_ds.to_netcdf(
-            save_directory + file_name, mode="w", format="NETCDF4", encoding=encoding
-        )
-# %%
+            for key in nc_global_attrs.keys():
+                to_save_ds.attrs[key] = nc_global_attrs[key]
+
+            flight_attrs = dicts.get_flight_attrs(a_filepaths[i][0])
+
+            for key in flight_attrs:
+                to_save_ds.attrs[key] = flight_attrs[key]
+
+            ###--------- Saving dataset to NetCDF file --------###
+
+            to_save_ds.to_netcdf(
+                save_directory + file_name, mode="w", format="NETCDF4", encoding=encoding
+            )
+    # %%
