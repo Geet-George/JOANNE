72a84259696e3ea9d683a408dbd65076772bdc55
Geet George <geetgeorge12@gmail.com>
2020-07-28 11:06:03 +0200

    add alt_bnds var

--- joanne/Level_3/Level_3.py
+++ joanne/Level_3/Level_3.py
@@ -21,7 +21,9 @@ reload(dicts)
 reload(f3)
 reload(joanne)
 # %%
-lv2_data_directory = "/Users/geet/Documents/JOANNE/Data/code_testing_data/"  # Level_2/"  # code_testing_data/"
+lv2_data_directory = (
+    "/Users/geet/Documents/JOANNE/Data/Level_2/"  # Level_2/"  # code_testing_data/"
+)
 
 lv3_dataset = f3.lv3_structure_from_lv2(lv2_data_directory)
 
@@ -44,6 +46,12 @@ for var in dicts.list_of_vars:
     f3.create_variable(
         to_save_ds, var, data=nc_data, dims=dicts.nc_dims, attrs=dicts.nc_attrs
     )
+
+to_save_ds["alt_bnds"] = xr.DataArray(
+    np.array([f3.interpolation_bins[:-1], f3.interpolation_bins[1:]]).T,
+    dims=["alt", "nv"],
+    coords={"alt": to_save_ds.alt.values},
+)
 
 file_name = (
     "EUREC4A_JOANNE_Dropsonde-RD41_" + "Level_3_v" + str(joanne.__version__) + ".nc"
