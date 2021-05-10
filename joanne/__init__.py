eaf05016298bbb60168aaf8a8c46c2a10442b0a8
Geet George <geetgeorge12@gmail.com>
2021-05-11 01:01:30 +0200

    add actual doi for data and software ready for v1.0.0

--- joanne/__init__.py
+++ joanne/__init__.py
@@ -3,6 +3,6 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-data_doi = "NOTREADY-10.25326/221"
-software_doi = "NOTREADY-10.5281/zenodo.4746313"
+data_doi = "10.25326/221"
+software_doi = "10.5281/zenodo.4746313"
 reference_study = "George et al., JOANNE : Joint dropsonde Observations of the Atmosphere in tropical North atlaNtic meso-scale Environments, ESSD, 2021"
